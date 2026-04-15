ad037bf8a24bc17c64b4846e36ff9d3351537ad5
DhanshriKharat1619 <dhanshrik1916@gmail.com>
2026-04-15 20:56:20 +0530

    Add files via upload

---
+++ custom-component.js
@@ -0,0 +1,42 @@
+const TextFieldComponent = Formio.Components.components.textfield;
+
+class MyCustomComponent extends TextFieldComponent {
+
+  static schema(...extend) {
+    return TextFieldComponent.schema({
+      type: 'mycustom',
+      label: 'My Custom Field',
+      key: 'mycustom',
+      ...extend
+    });
+  }
+
+  static get builderInfo() {
+    return {
+      title: 'My Custom Field',
+      group: 'basic',
+      icon: 'terminal',
+      weight: 70,
+      schema: MyCustomComponent.schema()
+    };
+  }
+
+  render() {
+    return super.render(`
+      <input type="text" class="form-control" placeholder="Enter value"/>
+    `);
+  }
+
+  attach(element) {
+    const input = element.querySelector('input');
+
+    input.addEventListener('input', (e) => {
+      this.setValue(e.target.value);
+    });
+
+    return super.attach(element);
+  }
+}
+
+// Register component
+Formio.Components.addComponent('mycustom', MyCustomComponent);
